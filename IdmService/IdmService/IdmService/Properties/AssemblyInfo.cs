ffa6d9436d6bd1dfb1b32ca951aa6078eb73c2a0
Doug Seelinger <dougseelinger@hotmail.com>
2016-10-20 16:32:35 -0400

    Converted to .NET 4.6.1/.NET Standard 1.4 (awaiting 2.0 availability)

--- IdmService/IdmService/IdmService/Properties/AssemblyInfo.cs
+++ IdmService/IdmService/IdmService/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IdmService")]
-[assembly: AssemblyDescription("Identity Manager Service assembly for working with Identity Manager SOAP web services.")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Doug Seelinger")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyProduct("IdmService")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("046d8576-2bbe-493c-a4d1-cd10d0fb5d1e")]
+[assembly: Guid("40340646-2a83-487a-9856-c7d35c75d4d0")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
